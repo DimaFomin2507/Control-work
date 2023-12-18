75fb57b5f109ec529abf3c6de229839ed8d91c9e
DimaFomin2507 <f.d.2006got@mail.ru>
2023-12-18 21:47:40 +0300

    1

--- 1.py
+++ 1.py
@@ -1,14 +1,74 @@
-"""
-Сделайте функцию, которая создаст новый список из этого, заменив None на 0
-"""
+# """
+# Сделайте функцию, которая создаст новый список из этого, заменив None на 0
+# """
+#
+# test_lst = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [None, 8, 9],
+#     [10, 11, 12],
+#     [13, None, 15],
+#     [16, 17, 18],
+#     [19, 20, 21],
+#     [22, 23, None],
+# ]
+#
+# # end_lst = []
+# # for item in test_lst:
+# #     middle_lst = []
+# #     for item2 in item:
+# #         middle_lst.append(
+# #             item2 if item2 is not None else 0
+# #         )
+# #     end_lst.append(middle_lst)
+# end_lst = [
+#     [
+#         item2 if item2 is not None else 0
+#         for item2 in item
+#     ]
+#     for item in test_lst
+# ]
+# print(end_lst)
 
-test_lst = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [None, 8, 9],
-    [10, 11, 12],
-    [13, None, 15],
-    [16, 17, 18],
-    [19, 20, 21],
-    [22, 23, None],
-]
+
+# class Cat:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def meow(self):
+#         print(f"{self.name}: Мяу!")
+# cat = Cat("Барсик")
+# cat2 = Cat("Антошка")
+# cat.meow()
+# cat2.meow()
+
+# class Student:
+#     def __init__(self, full_name, age, course):
+#         self.full_name = full_name
+#         self.age = age
+#         self.course = course
+#     def info(self):
+#         return (f"{self.full_name}: {self.age} лет, {self.course} курс")
+# dima = Student('Дмитрий', 22 , 2)
+# print(dima.info())
+
+class Field:
+
+    def __init__(self):
+        self.field = [' '] * 9
+    def _check(self, index):
+        return self.field[index] == ' '
+    def turn(self, index, symbol):
+        if self._check(index):
+            self.field[index] = symbol
+    def print(self):
+        print(self.field[:3])
+        print(self.field[3:6])
+        print(self.field[6:])
+        print()
+field = Field()
+field.print()
+field.turn(3, 'X')
+field.print()
+field.turn(3,'O')
+field.print()
--- 3.py
+++ 3.py
@@ -31,8 +31,16 @@ def test(*args):
         yield 1
 
 
+def solution(storages, count):
+    for rest_storages in range(storages, 0, -1):
+        c = count // rest_storages
+        count -= c
+        yield c
+
+
 def check(func, a, b):
+    print(list(func(a, b)))
     print(sum(list(func(a, b))))
 
 
-check(test, 5, 5)
+check(solution, 5, 97)
